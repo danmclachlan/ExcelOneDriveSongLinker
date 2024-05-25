28091a7fb4ad32d6ccf36db2169ec62deb96fb29
Dan McLachlan <drmclach@live.com>
2024-05-25 16:39:32 -0700

    Initial checkin of modified code based on the microsoftgraph/msgraph-sample-office-addin changed to create Read-Only anonymous links to files in a directory and then insert them into cells in an excel spreadsheet with the appropriate hyperlinks.  If the OneDrive is a shortcut then return the underlying url rather than a link to the file.

---
+++ src/addin/taskpane.js
@@ -0,0 +1,279 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+// <AuthUiSnippet>
+// Handle to authentication pop dialog
+/**
+ * @type {Office.Dialog | undefined}
+ */
+let authDialog = undefined;
+
+// @ts-ignore
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+var OfficeRuntime = OfficeRuntime || { auth: { getAccessToken: () => {throw new Error('office.js not loaded');}}};
+
+// Build a base URL from the current location
+function getBaseUrl() {
+  return location.protocol + '//' + location.hostname +
+  (location.port ? ':' + location.port : '');
+}
+
+// Process the response back from the auth dialog
+/**
+ * @param {{ message: string; origin: string | undefined; } | { error: number }} result
+ */
+function processConsent(result) {
+  // @ts-ignore
+  const message = JSON.parse(result.message);
+
+  authDialog?.close();
+  if (message.status === 'success') {
+    showMainUi();
+  } else {
+    const error = JSON.stringify(message.result, Object.getOwnPropertyNames(message.result));
+    showStatus(`An error was returned from the consent dialog: ${error}`, true);
+  }
+}
+
+// Use the Office Dialog API to show the interactive
+// login UI
+function showConsentPopup() {
+  const authDialogUrl = `${getBaseUrl()}/consent.html`;
+
+  Office.context.ui.displayDialogAsync(authDialogUrl,
+    {
+      height: 60,
+      width: 30,
+      promptBeforeOpen: false
+    },
+    (result) => {
+      if (result.status === Office.AsyncResultStatus.Succeeded) {
+        authDialog = result.value;
+        authDialog.addEventHandler(Office.EventType.DialogMessageReceived, processConsent);
+      } else {
+        // Display error
+        const error = JSON.stringify(result.error, Object.getOwnPropertyNames(result.error));
+        showStatus(`Could not open consent prompt dialog: ${error}`, true);
+      }
+    });
+}
+
+// Inform the user we need to get their consent
+function showConsentUi() {
+  $('.container').empty();
+  $('<p/>', {
+    class: 'ms-fontSize-24 ms-fontWeight-bold',
+    text: 'Consent for Microsoft Graph access needed'
+  }).appendTo('.container');
+  $('<p/>', {
+    class: 'ms-fontSize-16 ms-fontWeight-regular',
+    text: 'In order to access your files, we need to get your permission to access the Microsoft Graph.'
+  }).appendTo('.container');
+  $('<p/>', {
+    class: 'ms-fontSize-16 ms-fontWeight-regular',
+    text: 'We only need to do this once, unless you revoke your permission.'
+  }).appendTo('.container');
+  $('<p/>', {
+    class: 'ms-fontSize-16 ms-fontWeight-regular',
+    text: 'Please click or tap the button below to give permission (opens a popup window).'
+  }).appendTo('.container');
+  $('<button/>', {
+    class: 'primary-button',
+    text: 'Give permission'
+  }).on('click', showConsentPopup)
+    .appendTo('.container');
+}
+
+// Display a status
+/**
+ * @param {unknown} message
+ * @param {boolean} isError
+ */
+function showStatus(message, isError) {
+  $('.status').empty();
+  $('<div/>', {
+    class: `status-card ms-depth-4 ${isError ? 'error-msg' : 'success-msg'}`
+  }).append($('<p/>', {
+    class: 'ms-fontSize-24 ms-fontWeight-bold',
+    text: isError ? 'An error occurred' : 'Success'
+  })).append($('<p/>', {
+    class: 'ms-fontSize-16 ms-fontWeight-regular',
+    text: message
+  })).appendTo('.status');
+}
+
+/**
+ * @param {boolean} show
+ */
+function toggleOverlay(show) {
+  $('.overlay').css('display', show ? 'block' : 'none');
+}
+// </AuthUiSnippet>
+
+// <MainUiSnippet>
+function showMainUi() {
+  $('.container').empty();
+
+  $('<hr2/>', {
+    class: 'ms-fontSize-24 ms-fontWeight-semibold',
+    text: 'Select song to add'
+  }).appendTo('.container');
+  $('<hr2/>', {
+    class: 'ms-fontSize-12 ms-fontWeight-semibold',
+    text: ' (at ActiveCell)'
+  }).appendTo('.container');
+
+  // Create the input form
+  $('<form/>').on('submit', getSongLinks)
+    .append($('<label/>', {
+      class: 'ms-fontSize-16 ms-fontWeight-semibold',
+      text: 'Base Folder'
+    })).append($('<input/>', {
+      class: 'form-input',
+      type: 'text',
+      required: true,
+      id: 'baseFolder'
+    })).append($('<label/>', {
+      class: 'ms-fontSize-16 ms-fontWeight-semibold',
+      text: 'Song'
+    })).append($('<input/>', {
+      class: 'form-input',
+      type: 'text',
+      required: true,
+      id: 'songName'
+    })).append($('<input/>', {
+      class: 'primary-button',
+      type: 'submit',
+      id: 'importButton',
+      value: 'Add Song'
+    })).appendTo('.container');
+
+  $('<hr/>').appendTo('.container');
+}
+// </MainUiSnippet>
+
+// <getSongLinksSnippet>
+/**
+ * @param {{ preventDefault: () => void; }} evt
+ */
+async function getSongLinks(evt) {
+  evt.preventDefault();
+  toggleOverlay(true);
+
+  try {
+    const apiToken = await OfficeRuntime.auth.getAccessToken({ allowSignInPrompt: true });
+ 
+    const baseFolder = $('#baseFolder').val();
+    const songName = $('#songName').val();
+
+    const requestUrl =
+      `${getBaseUrl()}/graph/folderitemsurls?baseFolder=${baseFolder}&songName=${songName}`;
+
+    const response = await fetch(requestUrl, {
+      headers: {
+        authorization: `Bearer ${apiToken}`
+      }
+    });
+
+    if (response.ok) {
+      const files = await response.json();
+      if (files.length > 0) WriteUrlsToSheet(files);
+      showStatus(`Inserted ${files.length} song file links`, false);
+    } else {
+      const error = await response.json();
+      showStatus(`Error getting links from OneDrive: ${JSON.stringify(error)}`, true);
+    }
+
+    toggleOverlay(false);
+  } catch (err) {
+    console.log(`Error: ${JSON.stringify(err)}`);
+    showStatus(`Exception getting links from OneDrive: ${JSON.stringify(err)}`, true);
+  }
+}
+// <!<getSongLinksSnippet>
+
+// <WriteUrlsToSheetSnippet>
+/**
+ * @param {any[]} items
+ */
+async function WriteUrlsToSheet(items) 
+{
+  console.log(`in WriteUrlsToSheet: items count = ${items.length}`);
+  await Excel.run(async (context) => 
+  {
+    const cell = context.workbook.getActiveCell();
+    cell.load('address');
+    
+    await context.sync();
+
+    //get the full address of the active cell
+    var activeCellAddress = cell.address;
+
+    // calculate the range needed to insert the hyperlinks
+    var range = context.workbook.worksheets.getActiveWorksheet().getRange(activeCellAddress).getResizedRange(0, items.length - 1);
+    range.load('rowCount');
+    range.load('columnCount');
+    range.load('cellCount');
+    await context.sync();
+
+    console.log(range);
+
+    for (var i = 0; i < items.length; i++) {
+      let cell = range.getCell(0,i);
+      cell.hyperlink = {
+        address: items[i].WebUrl,
+        textToDisplay: items[i].Name
+      };
+      cell.values = items[i].Name;
+
+      range.format.autofitColumns();
+    }
+  }).catch (function(err) {
+    console.log(`Error: ${JSON.stringify(err)}`);
+    showStatus(err, true);
+  });
+}
+// </WriteUrlsToSheetSnippet>
+
+// <OfficeReadySnippet>
+Office.onReady(info => {
+  console.debug('in Office.onReady');
+  // Only run if we're inside Excel
+  if (info.host === Office.HostType.Excel) {
+    // eslint-disable-next-line no-undef
+    OfficeExtension.config.extendedErrorLogging = true; // better debugging
+    $(async function() {
+      let apiToken = '';
+      try {
+        apiToken = await OfficeRuntime.auth.getAccessToken({ allowSignInPrompt: true });
+        console.log(`API Token: ${apiToken}`);
+      } catch (error) {
+        console.log(`getAccessToken error: ${JSON.stringify(error)}`);
+        // Fall back to interactive login
+        showConsentUi();
+      }
+
+      // Call auth status API to see if we need to get consent
+      const authStatusResponse = await fetch(`${getBaseUrl()}/auth/status`, {
+        headers: {
+          authorization: `Bearer ${apiToken}`
+        }
+      });
+
+      const authStatus = await authStatusResponse.json();
+      if (authStatus.status === 'consent_required') {
+        showConsentUi();
+      } else {
+        // report error
+        if (authStatus.status === 'error') {
+          const error = JSON.stringify(authStatus.error,
+            Object.getOwnPropertyNames(authStatus.error));
+          showStatus(`Error checking auth status: ${error}`, true);
+        } else {
+          showMainUi();
+        }
+      }
+    });
+  }
+});
+// </OfficeReadySnippet>
---
+++ src/api/graph.ts
@@ -0,0 +1,192 @@
+// Copyright (c) Microsoft Corporation.
+// Licensed under the MIT License.
+
+import Router from 'express-promise-router';
+import * as graph from '@microsoft/microsoft-graph-client';
+import { Drive, DriveItem } from 'microsoft-graph';
+import 'isomorphic-fetch';
+import { getTokenOnBehalfOf } from './auth';
+import axios from 'axios';
+
+// <GetClientSnippet>
+async function getAuthenticatedClient(authHeader: string): Promise<graph.Client> {
+  const accessToken = await getTokenOnBehalfOf(authHeader);
+
+  return graph.Client.init({
+    authProvider: (done) => {
+      // Call the callback with the
+      // access token
+      done(null, accessToken || '');
+    },
+    debugLogging: true
+  });
+}
+// </GetClientSnippet>
+
+// <GetReadOnlyLinkSnippet>
+async function GetReadOnlyLink(client: graph.Client, id: string): Promise<string> {
+  const requestBody = {
+    type: 'view',
+    scope: 'anonymous'
+  };
+
+  const path = '/me/drive/items/';
+  const linkItem = await client
+    .api(path.concat(id, '/createlink'))
+    .post(requestBody);
+
+  // console.debug(`GetReadOnlyLink:${linkItem.link.webUrl}`);
+
+  return linkItem.link.webUrl;
+}
+// </GetReadOnlyLinkSnippet>
+
+// <ResolveShortcutSnippet>
+async function ResolveShortcut(client: graph.Client, id: string): Promise<string> {
+
+  const path = '/me/drive/items/';
+  const uriItem = await client
+    .api(path.concat(id, '?select=id,@microsoft.graph.downloadUrl'))
+    .get();
+
+  const downloadUrl = uriItem['@microsoft.graph.downloadUrl'];
+
+  let resolvedShortcut = '';
+
+  try {
+    const response = await axios.get(downloadUrl);
+    const fileContent = response.data; // content of the .url file
+
+    // extract the URL= line from the content
+    const urlLineMatch = fileContent.match(/URL=(.*)/i);
+
+    if (urlLineMatch) {
+      resolvedShortcut = urlLineMatch[1].trim();
+    } else {
+      console.error('URL= line not found in .url file content.');
+    }
+  } catch (error) {
+    console.error('Error downloading .url file:', error);
+  }
+
+  return resolvedShortcut;
+}
+// </ResolveShortcutSnippet>
+
+const graphRouter = Router();
+
+// <GetFolderitemurlsSnippet>
+interface ItemUrlObject {
+  Name: string;
+  Type: string;
+  WebUrl: string;
+}
+
+graphRouter.get('/folderitemsurls',
+  async function(req, res) {
+    const authHeader = req.headers['authorization'];
+
+    console.debug('in function for /folderitemsurls');
+
+    if (authHeader) {
+      try {
+        const client = await getAuthenticatedClient(authHeader);
+
+        //console.debug('after getAuthenticatedClient');
+        //console.debug(client);
+
+        const baseFolder = req.query['baseFolder']?.toString();
+        const songName = req.query['songName']?.toString();
+
+        console.debug(`baseFolder: ${baseFolder}, songName: ${songName}`);
+
+        const folderPath = baseFolder?.concat('/', songName || '');
+        const resultArray: ItemUrlObject[]  = [];
+
+        const drive: Drive = await client
+          .api('/me/drive')
+          .get();
+
+        if (drive === null) {
+          throw new Error('Could not get OneDrive root (/me/drive)');
+        }
+        //console.debug(drive);
+        
+        const basepath = '/drives/';
+        const driveItemList: DriveItem[] = [];
+        
+        const folderItem: DriveItem = await client
+          .api(basepath.concat(drive.id || '', '/root:/', folderPath || ''))
+          .get();
+
+        if (folderItem === null) {
+          throw new Error(`Could not get OneDrive folder (${folderPath})`);
+        }
+    
+        //console.debug(folderItem);
+
+        value = await GetReadOnlyLink(client, folderItem.id || '');
+        if (value !== undefined) {
+          resultArray.push({  'Name': folderItem.name || '', 'Type': 'Folder', 'WebUrl': value});  
+        }
+
+        const driveItemPage: graph.PageCollection = await client
+          .api(basepath.concat(drive.id || '', '/items/', folderItem.id || '', '/children'))
+          .get();
+        
+        // Set up a PageIterator to process the events in the result
+        // and request subsequent "pages" if there are more than 25
+        // on the server
+        const callback: graph.PageIteratorCallback = (driveItem) => {
+          driveItemList.push(driveItem);
+          return true;
+        };
+    
+        const iterator = new graph.PageIterator(client, driveItemPage, callback, undefined);
+        await iterator.iterate();
+
+        console.debug(`FileCount: ${driveItemList.length}`);
+    
+        for (let i = 0; i < driveItemList.length; i++) {
+          if (driveItemList[i].file !== null) {
+            // eslint-disable-next-line no-var
+            var value: string | undefined = undefined;
+
+            //console.debug(`driveItemList[${i}]:`);
+            //console.debug(driveItemList[i]);
+    
+            const id = driveItemList[i].id;
+            const name = driveItemList[i].name;
+            if (id !== null && id !== undefined) {
+              if (name !== null && name !== undefined && name.endsWith('.url')) {
+                value = await ResolveShortcut(client, id);
+                if (value !== '') {
+                  resultArray.push({  'Name': driveItemList[i].name || '', 'Type': 'Shortcut', 'WebUrl': value});
+                }
+              } else {
+                value = await GetReadOnlyLink(client, id);
+                if (value !== undefined) {
+                  resultArray.push({  'Name': driveItemList[i].name || '', 'Type': 'File', 'WebUrl': value});
+                }
+              }
+            } 
+          }
+        }
+        /**/
+        console.debug('resultArray: ');
+        console.debug(resultArray);
+
+        // Return the array of events
+        res.status(200).json(resultArray);
+      } catch (error) {
+        console.log(error);
+        res.status(500).json(error);
+      }
+    } else {
+      // No auth header
+      res.status(401).end();
+    }
+  }
+);
+
+export default graphRouter;
